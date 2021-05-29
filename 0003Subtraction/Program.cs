d16775f7986e39f5db75662f7121274cf37adc58
Barath <pbarath29@gamil.com>
2021-05-29 10:35:10 +0800

    * 0003Subtraction.csproj: operators
    * Program.cs:
    * Program.cs:
    * 0004Multiplication.csproj:

---
+++ 0003Subtraction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _0003Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int result;
+
+            Console.WriteLine("Enter a value for A");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for B");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a - b;
+            Console.WriteLine("Result is " + result);
+
+        }
+    }
+}
---
+++ 0004Multiplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _0004Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int result;
+
+            Console.WriteLine("Enter a value for A");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for B");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a * b;
+            Console.WriteLine("Result is " + result);
+        }
+    }
+}
