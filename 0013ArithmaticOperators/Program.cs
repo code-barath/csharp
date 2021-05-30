7c8e55b304b88c5ab9092e5f20a19535bc281e49
Barath <pbarath29@gamil.com>
2021-05-30 13:57:17 +0800

    decrimental operators

---
+++ 0013ArithmaticOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0013ArithmaticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0015DecOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _0015DecOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = 10;
+
+            //Console.WriteLine("Value of A before decrement " + a);
+            //a--;
+            //Console.WriteLine("Value of A after decrement " + a);
+
+            Console.WriteLine("Value of a is " + a--);
+            Console.WriteLine("Value of a is " + --a);
+
+            a -= 10; //a = a-10;
+            Console.WriteLine("value of a is " + a);
+        }
+    }
+}
