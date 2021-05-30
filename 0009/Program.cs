eb973d791fa74ea438fe47d47a41ec2b778c76ff
Barath <pbarath29@gamil.com>
2021-05-30 10:13:49 +0800

    * Program.cs:
    * 0010AreaofSquare.csproj: area of square

---
+++ 0009/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0010/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int area;
+
+            Console.WriteLine("Enter a value for the length");
+            length = Convert.ToInt32(Console.ReadLine());
+
+            area = (length * length);
+
+            Console.WriteLine("THE value of the square is " + area);
+        }
+    }
+}
