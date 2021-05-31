8079657f3925cdce4a45fed4917b161ef4b15129
Barath <pbarath29@gamil.com>
2021-05-31 14:46:46 +0800

    31-May-2021

--- 0007/Program.cs
+++ 0007/Program.cs
@@ -6,7 +6,19 @@ namespace _0007
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double triHeight;
+            double triBase;
+            double triArea;
+
+            Console.WriteLine("Enter the height of the triangle");
+            triHeight  = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value for the base");
+            triBase = Convert.ToDouble(Console.ReadLine());
+
+            triArea = (triHeight * triBase) / 2;
+            Console.WriteLine("Area of triangle is " + triArea);
+
         }
     }
 }
